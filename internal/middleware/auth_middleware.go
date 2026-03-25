273c54ddb00d05e3396f3fe0f848552613691e3c
deccersw <burovin123@yandex.ru>
2026-03-25 11:48:35 +0300

    added midleware auth

---
+++ internal/middleware/auth_middleware.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+	"todo_api/internal/config"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+func AuthMiddleware(cfg *config.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == "" || tokenString == authHeader {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid authorization header format"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if token.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+				return nil, fmt.Errorf("Unexpected signing method : %v", token.Header["alg"])
+			}
+			return []byte(cfg.JWTSecret), nil
+		})
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
+
+		Claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token Claims"})
+			c.Abort()
+			return
+		}
+
+		UserID, ok := Claims["user_id"].(string)
+
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token Claims"})
+			c.Abort()
+			return
+		}
+
+		if exp, ok := Claims["exp"].(float64); ok {
+			expiration := time.Unix(int64(exp), 0 )
+			time.Now().After(expiration){
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Token has expired"})
+				c.Abort()
+				return
+			}
+		}
+		c.Set("user_id", userID)
+		c.Next()
+	}
+
+}
